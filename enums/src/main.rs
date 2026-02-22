b28e63f460411e80dc5c5be87f6b73eeacc3ff60
firasastwani <firasastwani1@gmail.com>
2026-02-21 23:52:58 -0500

    learning enums

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,57 @@
+// can put any kind of data into these enum params (even structs or other enums)
+enum IpAddrKind {
+    V4(String),
+    V6(String),
+}
+
+enum Message {
+    Quit, 
+    Move {x: i32, y:i32},
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        // method body defined here
+    }
+}
+
+fn route(ip_kin: IpAddrKind) {
+
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let four = IpAddrKind::V4(String::from("127.0.0.1")); 
+
+    let m = Message::Write(String::from("hello"));
+    m.call(); 
+
+
+    // here its made into an option automatically because its callign "some";
+    let some_number = Some(5);
+    let some_char = Some('e');
+
+    let x: i8 = 5; 
+    let y: Option<i8> = Some(5);
+
+
+    let value = match y {
+        Some(v) => v, 
+        None => panic!("y was none"),
+    }; 
+
+    // shorthand
+    let value = y.expect("y was none");
+    let value = y.unwrap_or(0);
+
+    let sum = match y {
+        Some(v) => v + x,
+        None => panic!("why was none"),
+    }
+
+
+    // the type of option needs to be specified
+    let absent_number: Option<i32> = None; 
 }
